e3717d9d49bedcce4ddf78824ac088ecf888821d
Andres Orozco <andres.orozco97@hotmail.com>
2020-09-23 22:57:45 -0500

    Nuevas pruebas unitarias

--- src/app/image-details/image-details.component.spec.ts
+++ src/app/image-details/image-details.component.spec.ts
@@ -1,25 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ImageService } from '../services/image.service';
 
 import { ImageDetailComponent } from './image-details.component';
 
 describe('ImageDetailsComponent', () => {
   let component: ImageDetailComponent;
   let fixture: ComponentFixture<ImageDetailComponent>;
+  let mockService: ImageService = new ImageService();
+  let spy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageDetailComponent ]
+      declarations: [ ImageDetailComponent ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: () => 1,
+            },
+          },
+        },
+        { provide: ImageService, useValue: mockService }
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    spy = spyOn(mockService, 'getImage').and.returnValue({"id": 1, "brand": "perro", "url": "assets/images/perro1.jpg"});
+
     fixture = TestBed.createComponent(ImageDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('Debe crear el componente', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("Cuando la imagen existe debe retornar el json de la imagen", () => {
+    expect(component.image).toEqual({"id": 1, "brand": "perro", "url": "assets/images/perro1.jpg"});
+  });
+
+  it("Cuando se carga la vista, debe existir un elemento contenedor de imagenes con la clase img-container", () => {
+    expect(fixture.nativeElement.querySelector('.img-container')).toBeDefined();
+  });
+
 });
--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,27 +1,64 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FilterimagesPipe } from '../pipes/filterimages.pipe';
+import { ImageService } from '../services/image.service';
 
 import { GalleryComponent } from './image-gallery.component';
 
-describe('ImageGalleryComponent', () => {
+const Imagesdelatils = [    
+  { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
+  { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
+  { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
+  { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
+  { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" },
+] 
+
+describe('GalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
+  let mockService: ImageService = new ImageService();
+  let mockPipe: FilterimagesPipe = new FilterimagesPipe();
+  let spy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
+      declarations: [ GalleryComponent ],
+      providers: [
+        { provide: ImageService, useValue: mockService },
+        { provide: FilterimagesPipe, useValue: mockPipe }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    spy = spyOn(mockService, 'getImages').and.returnValues(Imagesdelatils);
+
     fixture = TestBed.createComponent(GalleryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // fit('should create', () => {
+  // it('Debe crear el componente', () => {
   //   expect(component).toBeTruthy();
   // });
+
+  // it('Cuando se carga la vista, debe existir un boton que contenga "All"', () => {
+  //   const btnAll = fixture.debugElement.nativeElement.querySelector('#All');
+  //   expect(btnAll.textContent).toContain('All');
+  //  });
+
+  // it('Cuando se carga la vista, debe existir un boton que contenga "Perro"', () => {
+  //   const btnPerro = fixture.debugElement.nativeElement.querySelector('#Perro');
+  //   expect(btnPerro.textContent).toContain('Perro');
+  //  });
+
+  // it('Cuando se carga la vista, debe existir un boton que contenga "Gato"', () => {
+  //   const btnGato = fixture.debugElement.nativeElement.querySelector('#Gato');
+  //   expect(btnGato.textContent).toContain('Gato');
+  //  });
+
+
+  
   // fit('should images be a ', () => {
   //   expect(component.images).toBeUndefined();
   // });
